99ac17712a673d3a6e09df6ca2cccdadb03e4b02
liandnsapatin <liandnsapatin@gmail.com>
2026-01-15 19:16:09 +0000

    very basic data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,14 @@
+# Liand Sapatin
 
+import pandas as pd
+
+# Read the csv file into variable
+df = pd.read_csv("avatar.csv")
+
+# Display first few rows in the dataframe
+print(df.head())
+
+# Put the book_num column into 
+avatar_count = df["book_num"]
+
+# Display 
