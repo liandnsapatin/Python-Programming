83aadb94302e22dd618d5a18f839af2aad44cb07
liandnsapatin <liandnsapatin@gmail.com>
2026-01-15 19:31:34 +0000

    started graph

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,7 @@
 # Liand Sapatin
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Read the csv file into variable
 df = pd.read_csv("avatar.csv")
@@ -8,7 +9,11 @@ df = pd.read_csv("avatar.csv")
 # Display first few rows in the dataframe
 print(df.head())
 
-# Put the book_num column into 
+# Put the book_num column into a variable
 avatar_count = df["book_num"]
 
-# Display 
+# Display largest year count
+# Print (year_count.max())
+
+largest_year_count = df [df['book_num'] == df ['book_num'].max()]
+print("Largest year o")
